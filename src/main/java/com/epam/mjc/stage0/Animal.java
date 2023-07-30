94087ae2429b25d1feaa6d8a1e05a52992b2af97
Makar <hoteng789@gmail.com>
2023-07-30 18:04:12 +0400

    Work on OOP pillars

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,26 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    private String color;
+    private  int numberOfPaws;
+    private  boolean hasFur;
+
+    public  Animal(){
+
+    }
+    public  Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        String fur = hasFur ? " and a fur.": " and no fur.";
+        String paws = numberOfPaws == 1? " paw": " paws";
+
+        return "This animal is mostly " +
+                 color + ". It has " +
+                 numberOfPaws + paws + fur;
+    }
+
 }
---
+++ src/main/java/com/epam/mjc/stage0/Main.java
@@ -0,0 +1,10 @@
+package com.epam.mjc.stage0;
+
+ class Main {
+     public static  void main(String[] args)
+     {
+         System.out.println( new Bird().getDescription());
+         System.out.println( new Dog().getDescription());
+     }
+
+}
